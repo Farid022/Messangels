8da95a0258c5d8ddab6c167a631164867d44daf4
Saad ibrahim <s3d.dev@gmail.com>
2022-03-29 09:54:53 +0500

    signup dob datepicker date range limit removed

--- Messangel/Views/Signup/SignupBirthView.swift
+++ Messangel/Views/Signup/SignupBirthView.swift
@@ -12,16 +12,16 @@ struct SignupBirthView: View {
     @EnvironmentObject var navigationModel: NavigationModel
     @State private var dob = Date()
     @State private var progress = 12.5
-    @State private var valid = false
     @State private var dobSelected = false
     @State private var editing = true
-    @State var offset : CGFloat = UIScreen.main.bounds.height
+    @State private var showAdultAgeAlert = false
+    @State private var offset : CGFloat = UIScreen.main.bounds.height
     @ObservedObject var userVM: UserViewModel
     @FocusState private var isFocused: Bool
     
     var body: some View {
         ZStack {
-            SignupBaseView(progress: $progress, valid: $valid, destination: AnyView(SignupPostcodeView(userVM: userVM)), currentView: "SignupBirthView", footer: AnyView(Text("Vous devez être majeur pour créer votre compte Messangel").font(.system(size: 13)))) {
+            SignupBaseView(progress: $progress, valid: .constant(!userVM.user.city.isEmpty && dob < Calendar.current.date(byAdding: .year, value: -18, to: Date())! && !userVM.user.dob.isEmpty), destination: AnyView(SignupPostcodeView(userVM: userVM)), currentView: "SignupBirthView", footer: AnyView(Text("Vous devez être majeur pour créer votre compte Messangel").font(.system(size: 13)))) {
                 Text("Je suis né(e) le")
                     .font(.system(size: 22))
                     .fontWeight(.bold)
@@ -46,9 +46,12 @@ struct SignupBirthView: View {
                     .submitLabel(.next)
                     .focused($isFocused)
             }
+            .alert(isPresented: $showAdultAgeAlert, content: {
+                Alert(title: Text("Error"), message: Text("Vous devez être majeur pour vous inscrire sur Messangel"))
+            })
             VStack {
                 Spacer()
-                CustomActionSheet(dob: $dob, dob_str: $userVM.user.dob, offset: $offset, isFocused: _isFocused, city: userVM.user.city)
+                CustomActionSheet(dob: $dob, dob_str: $userVM.user.dob, offset: $offset, showAdultAgeAlert: $showAdultAgeAlert, isFocused: _isFocused, city: userVM.user.city)
                     .onTapGesture {}
                     .offset(y: self.offset)
             }
@@ -58,14 +61,13 @@ struct SignupBirthView: View {
                 if dobSelected {
                     self.offset = UIScreen.main.bounds.height
                     userVM.user.dob = dateToStr(dob)
-                    self.validate()
+                    if dob > Calendar.current.date(byAdding: .year, value: -18, to: Date())! {
+                        showAdultAgeAlert = true
+                    }
                 }
             }
         }
         .animation(.default, value: offset)
-        .onChange(of: userVM.user.city) { value in
-            self.validate()
-        }
         .onDidAppear {
             if userVM.user.dob.isEmpty {
                 self.dob = Calendar.current.date(byAdding: .year, value: -18, to: Date())!
@@ -74,12 +76,7 @@ struct SignupBirthView: View {
             } else if let date = strToDate(userVM.user.dob) {
                 self.dob = date
             }
-            self.validate()
         }
-    }
-    
-    private func validate() {
-        self.valid = !userVM.user.city.isEmpty && dob < Calendar.current.date(byAdding: .year, value: -18, to: Date())! && !userVM.user.dob.isEmpty
     }
 }
 
@@ -87,12 +84,14 @@ struct CustomActionSheet : View {
     @Binding var dob: Date
     @Binding var dob_str: String
     @Binding var offset : CGFloat
+    @Binding var showAdultAgeAlert: Bool
     @FocusState var isFocused: Bool
     var city: String
     
     var body : some View{
         VStack(){
-            DatePicker(selection: $dob, in: ...Calendar.current.date(byAdding: .year, value: -18, to: Date())!, displayedComponents: .date) {}
+//            DatePicker(selection: $dob, in: ...Calendar.current.date(byAdding: .year, value: -18, to: Date())!, displayedComponents: .date) {}
+            DatePicker(selection: $dob, displayedComponents: .date) {}
                 .environment(\.locale, Locale(identifier: "fr"))
                 .datePickerStyle(WheelDatePickerStyle())
                 .labelsHidden()
@@ -102,6 +101,9 @@ struct CustomActionSheet : View {
                 dob_str = dateToStr(dob)
                 if city.isEmpty {
                     isFocused = true
+                }
+                if dob > Calendar.current.date(byAdding: .year, value: -18, to: Date())! {
+                    showAdultAgeAlert = true
                 }
             }) {
                 Text("OK")
